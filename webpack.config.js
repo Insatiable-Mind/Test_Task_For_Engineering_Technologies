6717b1d4d8fdcbe7e6d29665cc35ee377c7bb833
Insatiable_Mind <o.a.chernyakova@gmail.com>
2017-10-27 11:30:40 +0300

    add code for browsersync

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
 module.exports = {
   entry: './scripts/main.js',
 
@@ -10,4 +12,30 @@ module.exports = {
       { test: /\.js$/, loader: "babel-loader" }
     ]
   }
+
+  module: {
+    rules: [{
+        test: /\.less$/,
+        use: [{
+            loader: "style-loader"
+        }, {
+            loader: "css-loader"
+        }, {
+            loader: "less-loader"
+        }]
+    }]
+  }
+
+  plugins: [
+    new BrowserSyncPlugin(
+      {
+        host: 'localhost',
+        port: 3000,
+        proxy: 'http://localhost:3100/'
+      },
+      {
+        reload: false
+      }
+    )
+  ]
 };
