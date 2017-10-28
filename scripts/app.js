ec386c109fb59b8a090dbb93338ac3f40304000b
Insatiable_Mind <o.a.chernyakova@gmail.com>
2017-10-28 12:20:51 +0300

    change map type and add user location

--- scripts/app.js
+++ scripts/app.js
@@ -167,14 +167,18 @@ filter.addEventListener('reset', () => {
 "use strict";
 var map = L.map('map', {
   center: [51.505, -0.09],
-  zoom: 13
+  zoom: 13,
 });
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   maxZoom: 18,
-  id: 'mapbox.pirates',
-  accessToken: 'pk.eyJ1IjoiaW5zYXRpYWJsZS1taW5kIiwiYSI6ImNqOWIwaWdrNjFjdDIzM24ya21qbGJuMzQifQ.EIK16areNxtGW7AyBTug6A'
+  id: 'mapbox.streets',
+  accessToken: 'pk.eyJ1IjoiaW5zYXRpYWJsZS1taW5kIiwiYSI6ImNqOWIwaWdrNjFjdDIzM24ya21qbGJuMzQifQ.EIK16areNxtGW7AyBTug6A',
 }).addTo(map);
+
+map.locate({
+  setView: true,
+});
 
 
 /* unused harmony default export */ var _unused_webpack_default_export = (map);
