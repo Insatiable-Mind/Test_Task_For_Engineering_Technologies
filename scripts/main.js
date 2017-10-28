cde767d682902afc88695c4d040a117e315d1106
Insatiable_Mind <o.a.chernyakova@gmail.com>
2017-10-28 15:36:17 +0300

    set better zoom options for map

--- scripts/main.js
+++ scripts/main.js
@@ -84,18 +84,19 @@ filter.addEventListener('reset', () => {
 
 //*** MAP ***//
 const map = L.map('map', {
-  center: [55.751244, 37.618423],
-  zoom: 13,
+  center: [60.9333333, 76.5666667],
+  zoom: 2,
 });
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  maxZoom: 18,
+  maxZoom: 13,
   id: 'mapbox.streets',
   accessToken: 'pk.eyJ1IjoiaW5zYXRpYWJsZS1taW5kIiwiYSI6ImNqOWIwaWdrNjFjdDIzM24ya21qbGJuMzQifQ.EIK16areNxtGW7AyBTug6A',
 }).addTo(map);
 
 map.locate({
   setView: true,
+  maxZoom: 5,
 });
 
 showMapMarkers(data);
